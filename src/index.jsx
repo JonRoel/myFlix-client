20e1b49df40684b80ecdb4da2f310a17aad6dbf7
JonRoel <77934501+JonRoel@users.noreply.github.com>
2021-05-31 15:07:27 -0400

    created main view and movie view
    Created the main view and moview view

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-//Import statement to indicate that you need to bundle './index.scss'
+import MainView from "./components/main-view/main-view";
 
 import "./index.scss";
 
 //Main component
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
-    );
+    return <MainView />;
   }
 }
 
-//Finds the root if your app
+//Finds the root of the app
 const container = document.getElementsByClassName("app-container")[0];
 
 // Tells React to render your app in the root DOM element
